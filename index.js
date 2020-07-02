3b9c2d12bf6c8c94549c549b88ad4174ed67af14
Andrew Borovin <a@borovin.com>
2020-07-02 01:16:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const inp = fs.readFileSync('input.txt', 'utf-8').split('\n');
+const string = inp[0];
+const template = inp[1].split('').sort().join('');
+let charIndex = 0;
+let result = 0;
+let queue = [];
+
+while (charIndex < string.length) {
+  const currentChar = string[charIndex];
+  if (template.indexOf(currentChar) >= 0) {
+    queue.push(currentChar);
+  } else {
+    queue = [];
+  }
+
+  if (queue.length === template.length) {
+    if (queue.slice().sort().join('') === template) {
+      result++;
+    }
+    queue.shift();
+  }
+
+  charIndex++;
+}
+
+console.log(result);
